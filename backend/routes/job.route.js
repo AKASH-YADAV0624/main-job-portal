54fe4289c6c4fd754984fa1e79cfeb92abd746d0
AKASH-YADAV0624 <117830868+AKASH-YADAV0624@users.noreply.github.com>
2025-01-02 23:16:05 +0530

    Update job.route.js

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -5,9 +5,9 @@ import { deleteJob, duplicateJob, getAdminJobs, getAllJobs, getJobById, incremen
 const router=express.Router();
 
 router.route("/post").post(isAuthenticated,postJob);
-router.route("/get").get(isAuthenticated,getAllJobs);
+router.route("/get").get(getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated,getAdminJobs);
-router.route("/get/:id").get(isAuthenticated,getJobById);
+router.route("/get/:id").get(getJobById);
 router.route("/update/:id").put(isAuthenticated,updateJob);
 router.route("/delete/:id").delete(isAuthenticated,deleteJob);
 router.route('/duplicate/:id').post(isAuthenticated, duplicateJob);
