6d8a7088e5af08eadb7744f63d96909aeb93936f
AKASH-YADAV0624 <117830868+AKASH-YADAV0624@users.noreply.github.com>
2025-01-05 18:46:01 +0530

    Update JobDescription.jsx

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -149,7 +149,7 @@ const JobDescription=()=>{
         unitText: "MONTH",
       },
     },
-    datePosted: daysAgoFunction(singleJob?.createdAt)  || new Date().toISOString().split("T")[0],
+    datePosted: `${daysAgoFunction(singleJob?.createdAt)} days ago`  || new Date().toISOString().split("T")[0],
     validThrough: singleJob?.closingDate?.split("T")[0] || "2025-12-31T23:59:59",
     hiringOrganization: {
       "@type": "Organization",
